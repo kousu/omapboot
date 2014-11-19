5fc082efe7ea97b9dc52c55e86663f9f8f9f5b2a
kousu <nick@kousu.ca>
2014-11-19 13:05:59 -0500

    wtf; this gives 'i/o error' on the read(). but it should be identical to write.c: O_RDWR and nonblocking

--- write.py
+++ write.py
@@ -4,11 +4,15 @@ import struct
 messages = {"GET_ID": 0xF0030003, "BOOT": 0xF0030002}
 messages = {k: struct.pack("I", v) for k, v in messages.items()}
 
-for i in range(16):
+for i in [1]: # range(16):
 	try:
-		f = open("/dev/ugen0.%02d"%i, "wb", 0)
+		f = open("/dev/ugen0.%02d"%i, "r+b", 0)
 		oops = f.write(messages["GET_ID"])
 		print("wrote? %d bytes" % oops)
+		print(f)
+		durp = f.read(4)
+		print("got:")
+		print(repr(durp))
 	except Exception as e:
 		print("%d failed" %i)
 		print(e)
