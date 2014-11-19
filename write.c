756a932759c131a3654aaa26fe0ffc8100feaacb
kousu <nick@kousu.ca>
2014-11-19 14:12:38 -0500

    Use OpenBSD's USB_SET_SHORT_XFER to avoid the prissiness of the previous error.
    OpenBSD is so good, for what it is. They've thought of everything.
    This would be soooo frustrating on linux. I'd be stuck learning libusb.

--- write.c
+++ write.c
@@ -3,30 +3,39 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#include <dev/usb/usb.h>
+
 #define DEV "/dev/ugen0.01"
 
-int main() {
+int main() {  
   int f = open(DEV, O_RDWR | O_NONBLOCK);
   if(f == -1) {
     perror("open");
     return -1;
   }
+  
+  int r;
+  r = 1;
+  if((ioctl(f, USB_SET_SHORT_XFER, &r)) == -1) {
+    perror("ioctl: USB_SET_SHORT_XFER");
+    return -1;
+  }
+  
   char GET_ID[4] = {0x03, 0x00, 0x03, 0xF0};
-  int r = write(f, GET_ID, 4);
-  if(r == -1) {
+  if((write(f, GET_ID, 4)) == -1) {
     perror("write");
     return -2;
   }
   //sleep(1);
   
   unsigned char buf[1<<10];
-  r = read(f, buf, 81);
+  r = read(f, buf, sizeof(buf));
   if(r == -1) {
     perror("read");
     return -3;
   }
-  printf("read ASIC ID:\n");
-  for(int i=0; i<81; i++) {
+  printf("read ASIC ID (%d bytes):\n", r);
+  for(int i=0; i<r; i++) {
     printf("%02x ", buf[i]);
   }
   printf("\n");
