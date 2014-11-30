a3cbc2490a2fc5875138efb65fe7803dfaea935e
kousu <nick@kousu.ca>
2014-11-29 22:37:21 -0500

    [BROKEN] starting to patch in libusb support (via pyusb)

--- omapboot.py
+++ omapboot.py
@@ -24,12 +24,127 @@ import sys, os
 import fcntl, struct
 import time
 
+import warnings
 
-class bsd_ugen_bulk:
+class usb_bulk:
+    """
+    abstract base class
+    """
+    def __init__(self, vendor, product, endpoint=1, timeout=None):
+        """
+        endpoint is a 4-bit integer identifying which particular piece of the device this bulk port talks to
+        timeout is in milliseconds
+        """
+        if not (0 <= endpoint < (1<<4)):
+            raise ValueError("USB endpoints are only 4 bits long")
+        if endpoint == 0:
+            raise ValueError("Control endpoints are never bulk endpoints.") #TODO choose a better exception class
+        
+        if timeout == 0:
+            # be defensively consistent with bsd_ugen_bulk
+            timeout = None
+        
+        if timeout is not None:
+            if not (0 < timeout < 1<<32):
+                raise ValueError("Timeout must be an unsigned 32-bit integer if given.")
+        
+        self.device = None
+        self.endpoint = endpoint
+        self.timeout = timeout
+    
+    def read(self, len):
+        raise NotImplementedError
+    
+    def write(self, data):
+        raise NotImplementedError
+    
+    def close(self):
+        raise NotImplementedError
+
+class pyusb_bulk(usb_bulk):
+    """
+    
+    """
+    
+    import usb as _usb
+    
+    def __init__(self, vendor, product, endpoint=1, timeout=None):
+        """
+        
+        timeout is in milliseconds; if it's 
+        the
+        """
+        super().__init__(vendor, product, endpoint, timeout)
+        
+        self._dev = self._usb.core.find(idVendor=vendor, idProduct=product)
+        if self._dev is None:
+            raise OSError("Unable to find USB Device %04x:%04x" % (vendor, product))
+        
+        #XXX does this need to "claim" the "interface"? it seems to work without doing that...
+        # the pyusb docs suggest that doing this is the correct first step
+        # this isn't quite claiming, but maybe it's.. the same?
+        self._dev.set_configuration()
+        
+        # set up the in and out endpoints by bit-twiddling
+        # see http://www.beyondlogic.org/usbnutshell/usb5.shtml#EndpointDescriptors for a reference
+        # Endpoint Address
+        # Bits 0..3b Endpoint Number.
+        # Bits 4..6b Reserved. Set to Zero
+        # Bits 7 Direction 0 = Out, 1 = In (Ignored for Control Endpoints)
+        # It seems like this should be something handled by pyusb already.
+        print(self._dev)
+        
+        # motherfuckers
+        # it would be convenient to cache the Endpoints at init
+        # but the arg to Endpoint() is a "logical index" which is basically meaningless:
+        # it's the index of the endpoint in dev.configurations()[0].interfaces()[0].endpoints()
+        # rather than being, say, the USB endpoint ID or at least the bEndpointAddress
+        # TODO: patch pyusb so that you write Endpoint(device, endpoint_id, direction) instead
+        #
+        self._endpoint = endpoint
+        self._in = self._usb.core.Endpoint(self._dev, 0b10000000 | endpoint)
+        self._out = self._usb.core.Endpoint(self._dev, 0b00000000 | endpoint)
+        
+    def read(self, len):
+        """
+        returns a bytes object
+        """
+        return self._in.read(len, timeout=self.timeout).tobytes()
+        #pyusb works in Array objects. Which are totally the future,
+        # but are inconsistent with bsd_ugen_bulk
+        # and are more obscure than common python.
+    
+    def write(self, data):
+        """
+        data should be a bytes object
+        """
+        return self._out.write(data, timeout=self.timeout) #careful: pyusb returns array objects, but because of the magic of iterators and polymorphism, *hidden inside this call*, data can be a bytes
+    
+    def close(self):
+        del self._in
+        del self._out
+        
+        # *explicitly run the closing code*, which is in usb.core.Device.__del__(),
+        # and then defang it so that the gc doesn't get pissy that we've done its work already
+        self._dev.__del__()
+        self._dev.__del__ = lambda: pass
+        # pyusb should use a context manager instead of mucking about with __del__s for this
+        # __del__ is *not* the opposite of __init__ and it's *not* the same as a C++ dectructor
+        del self._dev 
+    
+
+class bsd_ugen_bulk(usb_bulk):
     """
     wrap BSD's ugen(4) device into a file-like python class.
     
-    This class is only good for. If you try to use it on a non-bulk endpoint, the open() will fail.
+    This class is only good for bulk. If you try to use it on a non-bulk endpoint, the open() will fail.
+    
+    Online reference: http://www.openbsd.org/cgi-bin/man.cgi/OpenBSD-current/man4/ugen.4?query=ugen
+    
+    TODO:
+    * [ ] make .timeout() a property that calls ._setTimeout();
+         this is complicated by this chicken-or-egg: self._dev needs to be exist to run _setTimeout, but self.timeout is set by super().__init__() before that happens
+         i suppose i could call super().__init__ in the middle of things, but that's... weird
     """
     
     # ioctl codes extracted by writing a C program that #include <dev/usb/usb.h> and printf'ing.
@@ -37,15 +152,24 @@ class bsd_ugen_bulk:
     USB_SET_SHORT_XFER = 0x80045571 
     USB_SET_TIMEOUT = 0x80045572
 
-    def __init__(self, device, endpoint):
+    def __init__(self, vendor, product, endpoint=1, timeout=None):
         """
+        
+        endpoint defaults to '1' because that's the most common endpoint to have a bulk interface.
         TODO: poll ugens for their vendor:device id so that the API can be "open such and such device"
         """
+        super().__init__(vendor, product, endpoint, timeout)
+        
+        warnings.warn("bsd_ugen_bulk does not implement device and endpoint scanning yet; it just assumes that you only have one USB device you want to work with at a time.")
+        #TODO: instead we should scan the devices with ioctl()s on /dev/ugen$I.00 for (VENDOR, PRODUCT)
+        device = 0
+        
         # rewrite (device: USB int id, endpoint: USB int id) to (device: unix device)
         device = "/dev/ugen%d.%02d" % (device, endpoint)
         
         self._dev = open(device, "wb+", 0)
         self._setShortTransfer()
+        self._setTimeout(self.timeout)
     
     def read(self, len):
         return self._dev.read(len)
@@ -84,12 +208,15 @@ class bsd_ugen_bulk:
     def _setTimeout(self, timeout):
         """
         """
+        if timeout is None:
+            #  "The value 0 is used to indicate that there is no timeout."
+            timeout = 0 #
         fcntl.ioctl(self._dev, self.USB_SET_TIMEOUT,
                     struct.pack("I", timeout)) #<-- we have to 'struct.pack' because ioctl always expects a *pointer*, even if it's just a pointer to an int which it doesn't modify. python's ioctl handles this by taking bytes() objects, extracting them to C buffers temporarily, and returning the value of it after the C ioctl() gets done with it in a new bytes() object
 
 
 usb_bulk = bsd_ugen_bulk #future-proofing that I'll probably never use
-
+usb_bulk = pyusb_bulk
 
 
 def readinto_io(self, target, chunksize=4096):
@@ -130,23 +257,21 @@ class OMAP4:
     del messages
     
     VENDOR = 0x0451
-    PRODUCT = 0xd00f #TODO: this needs to be a list
+    PRODUCT = 0xd00f #TODO: this needs to be a list; XXX pyusb has hooks that make it easy to implement this... but openbsd doesn't
 
     def __init__(self, block=True):
         """
         
         block: whether to fail if the device is not available, or wait for it to become available.
         """
-        #TODO: get (device, endpoint) from querying the OS's USB stack for (VENDOR, PRODUCT)
-        device, endpoint = 0, 1
         
         if block:
-            # really, this should happen above when we *query* for the device
-            # but querying is, at best, an expensive poll under ugen(4)
-            # So I'm stuck with polling:
+            # As far as I can tell, without kernel hooks (which are too
+            # platform-specific for this code) USB has no way to register
+            # event handlers. So I'm stuck with polling:
             while True:
                 try:
-                    usb_bulk(device, endpoint).close()
+                    usb_bulk(self.VENDOR, self.PRODUCT).close()
                     break
                 except OSError:
                     pass
