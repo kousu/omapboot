a0765cbf9958c96db743bf100787d9a3f9263418
kousu <nick@kousu.ca>
2014-12-11 09:40:29 -0500

    Parse the ASIC ID, now that I have the reference in hand.
    I used array() here because the subblock format is a header+variable-length format,
    and that's shortest to parse destructively: with .pop()s.

--- OMAP.py
+++ OMAP.py
@@ -2,6 +2,14 @@
 
 import os
 import struct
+from array import array
+
+# monkey-patch array to prettyprint a *byte* array in hex
+def tohex(self):
+    assert self.typecode == "B", "Only valid on byte arrays"
+    return "0x"+str.join('', ("%02X" % e for e in self))
+#array.tohex = tohex; del tohex #darn. array is a C extension type so I can't do this because python's a jerk.
+
 
 import time
 
@@ -18,9 +26,12 @@ class OMAP4(BaseOMAP):
     
     """
     
-    
-    # notable points:
-    # * the reference I've been working from, besides guesswork, is <https://gforge.ti.com/gf/project/flash>/trunk/omapflash/host/pheriphalboot.c
+    # References:
+    # * TI's OMAP4430 Technical Reference Manual (TRM) Chapter 27: Initialization, available at <http://www.ti.com/product/OMAP4430/technicaldocuments>
+    # * TI's Flash.exe/omapflash.exe, especially <https://gforge.ti.com/gf/project/flash>/trunk/omapflash/host/pheriphalboot.c
+    # * guesswork
+    #
+    # Notes:
     # * the protocol is little-endian *for all currently defined devices*, (see: ti's flash[...])
     #   and it's probably not going to change now that TI's given up on it
     
@@ -62,7 +73,54 @@ class OMAP4(BaseOMAP):
         
         closes the USB device when done, since booting means replacing what this class is designed to talk to
         """
+        self._dev.write(self.GET_ID)
+        ASIC = self._dev.read(0xFF)
+
+        def split_ASIC_blocks(ASIC):
+            ASIC = array("B", ASIC)
+            N = ASIC.pop(0)
+            for i in range(N):
+                type = ASIC.pop(0)
+                length = ASIC.pop(0)
+                assert length <= len(ASIC), "Don't overflow! Python will silently let us overflow if we're not careful"
+                data, ASIC = ASIC[:length], ASIC[length:]
+                assert data.pop(0) == 1, "Fixed value, e.g. as in TRM table 27-19"
+                yield type, data
+            assert not ASIC, "ASIC should be empty once we've parsed all its blocks"
         
+        def parse_ASIC_blocks(ASIC):
+            for type, data in split_ASIC_blocks(ASIC):
+                # TODO: clean this up with some classes and a nice dictionary lookup
+                #print(type, data) #DEBUG
+                if type == 0x01:
+                    assert len(data) == 4
+                    model, ch_enabled, version = data[:2], data[2], data[3]
+                    assert model.tobytes() == b"\x44\x30", "model number, written in hex just to be funny"
+                    ch_enabled = {0x07: "enabled", 0x17: "disabled"}.get(ch_enabled, "unknown")
+                    print("Model:", tohex(model)[2:])
+                    print("ROM revision: 0x%02x" % (version,))
+                    print("CH:", ch_enabled) #this has something to do with the header format of certain boot images. See the TRM.
+                
+                ## these next ones were taken from @swetland's usbboot.c. I don't know what they mean.
+                elif type == 18:
+                   assert len(data) == 20
+                   print("IDEN:", tohex(data))
+                elif type == 19:
+                    # unknown and undocumented
+                    assert len(data) == 1
+                    print("Undocumented ID subblock 18: %02X"  % (data[0],))
+                elif type == 20:
+                    assert len(data) == 32
+                    print("MPKH:", tohex(data))
+                elif type == 21:
+                    assert len(data) == 8
+                    CRC0, CRC1 = data[:4], data[4:]
+                    print("CRC0:", tohex(CRC0))
+                    print("CRC0:", tohex(CRC1))
+        
+        print("recevied ASIC ID banner:")
+        parse_ASIC_blocks(ASIC)
+
         # upload 2nd stage (x-loader) via the 1st stage
         self._dev.write(self.BOOT)
         print("Uploading x-loader...", end="", flush=True);
